19fcd8ee83c6301a73b85ef635999a0a5ddb5aea
Evi1Russian <orekhov.work1@gmail.com>
2022-06-24 13:12:02 +0300

    Praktikum bot v.2

--- homework.py
+++ homework.py
@@ -51,7 +51,6 @@ def send_message(bot, message):
 
 def get_api_answer(current_timestamp):
     """Делает запрос к API и возвращает ответ."""
-
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
     response = requests.get(ENDPOINT, headers=HEADERS, params=params)
